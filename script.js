073419a9a2d7715650f4eb069651bd693f6d52ac
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-11 20:51:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstChar(text) {
   // your code here
+	 str = str.trim();
+
+    // Check if the trimmed string is empty or consists only of spaces
+    if (str === "") {
+        return "";
+    }
+
+    // Return the first character of the trimmed string
+    return str[0];
 }
 
 // Do not change the code below
